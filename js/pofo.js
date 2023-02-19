1142784b7bc0da5bc6cabf156dcc72730155b826
mazen safar <anasmazenabusafar@gmail.com>
2023-02-19 13:46:01 +0200

    commit changes

---
+++ js/pofo.js
@@ -0,0 +1,81 @@
+// header backgraound black
+window.onscroll = function () {
+    const h = document.querySelector('#ff');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        h.classList.add('bg')
+    } else {
+        h.classList.remove('bg')
+    }
+}
+
+// fade fron bottom
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", reveal);
+
+// fade in
+function reveal_i() {
+    var reveals = document.querySelectorAll(".reveal-i");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", reveal_i);
+
+// fade reight
+function reveal_r() {
+    var reveals = document.querySelectorAll(".reveal-r");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", reveal_r);
+
+// fade left
+function reveal_l() {
+    var reveals = document.querySelectorAll(".reveal-l");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", reveal_l);
